e67f7d4178768d3d817fa22e095e9dad431ad8b6
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-10-25 13:15:52 +0100

    added test case

--- src/test/java/uk/gov/hmcts/payments/DemoUnitTest.java
+++ src/test/java/uk/gov/hmcts/payments/DemoUnitTest.java
@@ -1,13 +1,29 @@
 package uk.gov.hmcts.payments;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.payments.controllers.RootController;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 class DemoUnitTest {
+
+    @Autowired
+    RootController rootController;
 
     @Test
     void exampleOfTest() {
         assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
+    }
+
+    @Test
+    void testRootController() {
+        ResponseEntity<String> response = rootController.welcome();
+        assertEquals(200,response.getStatusCode().value());
+        assertEquals("Welcome to CPO update service",response.getBody());
     }
 }
