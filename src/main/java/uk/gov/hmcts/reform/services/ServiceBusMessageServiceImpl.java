b487bbdc39c852deb8be0640c097ead3af6e59cd
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2022-05-12 17:21:43 +0100

    Update ServiceBusMessageServiceImpl.java

--- src/main/java/uk/gov/hmcts/reform/services/ServiceBusMessageServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/services/ServiceBusMessageServiceImpl.java
@@ -27,7 +27,8 @@ public class ServiceBusMessageServiceImpl implements ServiceBusMessageService {
         CpoUpdateServiceRequest cpoUpdateServiceRequest;
         try {
             cpoUpdateServiceRequest = mapper.readValue(body.get(0), CpoUpdateServiceRequest.class);
-            if (cpoUpdateServiceRequest.getAction().equalsIgnoreCase("DLQTest")){
+            if (cpoUpdateServiceRequest.getAction().equalsIgnoreCase("DLQTest"))
+            {
                 throw new InvalidCpoUpdateRequestException("Internal Server Error", HttpStatus.SERVICE_UNAVAILABLE, new IOException());
             }
         } catch (Exception e) {
