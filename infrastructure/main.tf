a37a80d4c1e456772e2ca540b56b73b77dc17c3e
David Jones <dave.jones@hmcts.net>
2024-09-04 14:51:33 +0100

    Forgot to remove old code - Pipeline breaking.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -46,42 +46,6 @@ resource "azurerm_key_vault_secret" "ccpay_service_request_cpo_update_topic_prem
   key_vault_id = data.azurerm_key_vault.ccpay_key_vault.id
 }
 
-######################################
-# START Old Service Bus Namespace
-# TBD: Remove this block after migration
-######################################
-
-data "azurerm_servicebus_namespace" "ccpay_servicebus_namespace" {
-  name                = join("-", [var.product, "servicebus", var.env])
-  resource_group_name = join("-", [var.product, var.env])
-}
-
-module "service_request_cpo_update_topic" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-topic"
-  name                  = "ccpay-service-request-cpo-update-topic"
-  namespace_name        = data.azurerm_servicebus_namespace.ccpay_servicebus_namespace.name
-  resource_group_name   = data.azurerm_resource_group.rg.name
-}
-
-module "service_request_cpo_update_subscription" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription"
-  name                  = local.subscription_name
-  namespace_name        = data.azurerm_servicebus_namespace.ccpay_servicebus_namespace.name
-  resource_group_name   = data.azurerm_resource_group.rg.name
-  topic_name            = module.service_request_cpo_update_topic.name
-  depends_on            = [module.service_request_cpo_update_topic]
-}
-
-resource "azurerm_key_vault_secret" "ccpay_service_request_cpo_update_topic_shared_access_key" {
-  name         = "ccpay-service-request-cpo-update-topic-shared-access-key"
-  value        = module.service_request_cpo_update_topic.primary_send_and_listen_shared_access_key
-  key_vault_id = data.azurerm_key_vault.ccpay_key_vault.id
-}
-
-######################################
-# END Old Service Bus Namespace
-######################################
-
 data "azurerm_key_vault" "s2s_key_vault" {
   name                = local.s2s_key_vault_name
   resource_group_name = local.s2s_vault_resource_group
