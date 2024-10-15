d4fe1c57b50ce8f0218547adf20ad1f2f2ca75c1
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2024-10-15 17:22:27 +0100

    CME-106 update to Spring Boot 3

--- src/main/java/uk/gov/hmcts/reform/exceptions/MaxTryExceededException.java
+++ src/main/java/uk/gov/hmcts/reform/exceptions/MaxTryExceededException.java
@@ -1,15 +1,15 @@
 package uk.gov.hmcts.reform.exceptions;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 public class MaxTryExceededException  extends RuntimeException {
     private String server;
-    private HttpStatus status;
+    private HttpStatusCode status;
     public static final long serialVersionUID = 333297431;
 
-    public MaxTryExceededException(String server, HttpStatus status, Throwable cause) {
+    public MaxTryExceededException(String server, HttpStatusCode status, Throwable cause) {
         super(cause);
         this.server = server;
         this.status = status;
